c055b6aaf2b9b7322a49e9a3b2bd1f3822271e58
Marioccarneiro <mario.ccarneiro@hotmail.com>
2023-05-15 22:23:36 -0300

    props-template

--- src/App.js
+++ src/App.js
@@ -2,9 +2,58 @@ import Garagem from "./Componentes/Garagem";
 import "./styles.css";
 
 export default function App() {
+  const nome = "Mario";
+  const sobrenome = " Carneiro";
+
+  const apresentaGaragem = (nome) => alert("Boas vindas a garagem de " + nome);
+  // alert(`Boas vindas a garagem de ${nome}`)
+
+  const nome2 = "Karina";
+
+  const dadosCarro = {
+    nome: "Twingo",
+    ano: 2003,
+    cor: "Verde Abacate",
+    flex: false
+  };
+
+  const dadosCarro2 = {
+    nome: "Fusca",
+    ano: 1995,
+    cor: "Azul",
+    flex: true
+  };
+
+  const dadosCarro3 = {
+    nome: "Onix",
+    ano: 2019,
+    cor: "Vermelho",
+    flex: true
+  };
+
+  const dadosCarro4 = {
+    nome: "Crossfox",
+    ano: 2010,
+    cor: "Amarelo",
+    flex: false
+  };
+
   return (
-    <div>
-      <Garagem />
+    <div className="App">
+      {/* <Garagem
+        nome={nome}
+        sobrenome={sobrenome}
+        apresentaGaragem={apresentaGaragem}
+        dadosCarro={dadosCarro}
+      /> */}
+      <Garagem
+        nome={nome2}
+        apresentaGaragem={apresentaGaragem}
+        dadosCarro={dadosCarro}
+        dadosCarro2={dadosCarro2}
+        dadosCarro3={dadosCarro3}
+        dadosCarro4={dadosCarro4}
+      />
     </div>
   );
 }
